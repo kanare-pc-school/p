bd998ab108e4e359d2f80fdf9754d2ac4047a1f9
quizdy <quizdynet@gmail.com>
2022-04-09 01:15:57 +0900

    いろ変更

--- index.js
+++ index.js
@@ -15,8 +15,14 @@ $('#send').on('click', (_e) => {
 })
 
 socket.on('receiveMsg', (user, msg) => {
-    const tag = '<div class="char animate__animated animate__bounceInDown tooltip">' + msg + '<span class="tooltip-text">' + user + '</span></div>'
-    $('#show').append(tag)
+    if ($('#hide').prop('checked')) {
+        const tag = '<div class="char hide animate__animated animate__bounceInDown tooltip">' + msg + '<span class="tooltip-text" style="background-color: ' + getColor(user) + '">' + user + '</span></div>'
+        $('#show').append(tag)
+    } else {
+        const tag = '<div class="char animate__animated animate__bounceInDown tooltip">' + msg + '<span class="tooltip-text" style="background-color: ' + getColor(user) + '">' + user + '</span></div>'
+        $('#show').append(tag)
+    }
+
 })
 
 $('#user').on('input', (e) => {
@@ -26,3 +32,17 @@ $('#user').on('input', (e) => {
 $('#trash').on('click', (_e) => {
     $('#show').empty()
 })
+
+$('#hide').on('click', (e) => {
+    if ($(e.currentTarget).prop('checked')) {
+        $('.char').addClass('hide')
+    } else {
+        $('.char').removeClass('hide')
+    }
+})
+
+function getColor(user) {
+    const n = Array.from(user).map(ch => ch.charCodeAt(0)).reduce((a, b) => a + b)
+    const colorAngle = (n * n) % 360
+    return `hsl(${colorAngle}, 80%, 64%)`
+}
