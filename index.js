81017bf7aa2c49a528742170bda40152b9510358
kanare-pc-school <52237784+kanare-pc-school@users.noreply.github.com>
2022-04-08 17:36:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ $('#send').on('click', (_e) => {
 })
 
 socket.on('receiveMsg', (user, msg) => {
-    const tag = '<div class="char animate__animated animate__bounceInDown">' + msg + '</div>'
+    const tag = '<div class="char animate__animated animate__bounceInDown">' + msg + user + '</div>'
     $('#show').append(tag)
+})
+
+$('#user').on('input', (e) => {
+    $('#header>img').attr('src', 'https://avatars.dicebear.com/api/pixel-art/' + $(e.currentTarget).val() + '.svg')
 })
